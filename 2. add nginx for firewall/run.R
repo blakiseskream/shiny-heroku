9753e2eaa08463963f4d706c27d17c4c56ef0ac0
Andrew Watson <andrew.watson@salesforce.com>
2018-05-08 16:07:42 -0700

    more stuff

--- 2. add nginx for firewall/run.R
+++ 2. add nginx for firewall/run.R
@@ -6,3 +6,24 @@
 # Created by blakiseskream on 5/7/2018
 # MIT License and shit
 #
+
+library(shiny)
+
+# When we run nginx we need to change R to point to the /app folder manually
+setwd('/app')
+
+# Create a file at the path /tmp/app-initalized
+# This tells nginx that the app is running, and it can now forward traffic to the custom 'port' below
+# If this file is created, nginx will simply wait forever until the app says its running
+file.create("/tmp/app-initialized")
+port <- '/tmp/nginx.socket'
+attr(port, 'mask') <- strtoi("117", 8)
+
+
+# Runs the app in the working directory. By default this will search for an `app.R` file or 
+# a ui.R + server.R file combination
+# In this case the port is now the address to the nginx proxy
+shiny::runApp(
+  appDir = getwd(),
+  port = port
+)
--- 3. add OAuth/run.R
+++ 3. add OAuth/run.R
@@ -1,65 +1,59 @@
-#
-# run.R
-# 3. Add OAuth
-# shiny-heroku
-#
-# Created by blakiseskream on 5/7/2018
-# MIT License and shit
-#
+# TODO: To get this to work on Heroku you will need to fill out the info on lines
+# 35, set APP_URL to your heroku apps url 
+# 40, set key = to the Github application's Client ID
+# 41, set secret = to the Github application's Client Secret
+# Create a Github OAuth app here https://github.com/settings/applications/new
 
 
 library(shiny)
 library(httr)
 
-RunMyShinyApp <- function() {
+RunHerokuApp <- function() {
   # OAuth setup --------------------------------------------------------
-  
-  # Most OAuth applications require that you redirect to a fixed and known
+  # (1) Most OAuth applications require that you redirect to a fixed and known
   # set of URLs. Many only allow you to redirect to a single URL: if this
   # is the case for, you'll need to create an app for testing with a localhost
   # url, and an app for your deployed app.
   
   if (interactive()) {
-    # testing url
-    source("environment.R", local = TRUE)
-    source("server.R", local = TRUE)
-    source("ui.r", local = TRUE)
+    # testing url and token
+    options(shiny.port = 8100)
     APP_URL <- "http://localhost:8100/"
+    
+    # (2) Note that secret is not really secret, and it's fine to include inline
+    app <- oauth_app("shiny-local-app",
+                     key = "51d46f96810d1fd182a2",
+                     secret = "66eec8782825eeb61007dbef32f91afc9c3587aa",
+                     redirect_uri = APP_URL
+    )
     
   } else {
     
-    # deployed URL
-    setwd("/app")
-    source("server.R", local = TRUE)
-    source("ui.r", local = TRUE)
-    APP_URL <- Sys.getenv("APP_URL")
+    setwd('/app')
+    
+    # deployed URL and token
+    APP_URL <- "https://my-app-name.herokuapp.com/" # NOTE: SET THIS TO YOUR APP'S URL
+    
+    # (2) Note that secret is not really secret, and it's fine to include inline
+    # NOTE: SET THIS BASED ON YOUR GITHUB APP.
+    app <- oauth_app("my-heroku-app", 
+                     key = "MY_APP_ID",
+                     secret = "MY_SECRET_KEY",
+                     redirect_uri = APP_URL
+    )
   }
   
-  # Note that secret is not really secret, and it's fine to include inline
+  # (3) Here I'm using a canned endpoint, but you can create with oauth_endpoint()
+  api <- oauth_endpoints("github")
   
-  # SALESFORCE
-  # TODO: MOVE TO ENIVORNMENT VARIABLES
-  sfdcId <- Sys.getenv("APP_ID")
-  sfdcSecret <- Sys.getenv("CLIENT_SECRET")
-  
-  app <- oauth_app("sfdc-dawnstar",
-                   key = sfdcId,
-                   secret = sfdcSecret,
-                   redirect_uri = APP_URL
-  )
-  
-  # Here I'm using a canned endpoint, but you can create with oauth_endpoint()
-  sfdcAPI <- oauth_endpoint(
-    authorize = "https://login.salesforce.com/services/oauth2/authorize"
-    , access    = "https://login.salesforce.com/services/oauth2/token"
-  )
-  
-  # Always request the minimal scope needed. For github, an empty scope
+  # (4) Always request the minimal scope needed. For github, an empty scope
   # gives read-only access to public info
-  #scope <- "https://www.googleapis.com/auth/userinfo.profile"
-  scope <- "id"
+  scope <- ""
   
   # Shiny -------------------------------------------------------------------
+  
+  # Bring in the server.R and ui.R objects
+  source('app.R')
   
   has_auth_code <- function(params) {
     # params is a list object containing the parsed URL parameters. Return TRUE if
@@ -69,71 +63,68 @@ RunMyShinyApp <- function() {
     return(!is.null(params$code))
   }
   
-  # A little-known feature of Shiny is that the UI can be a function, not just
+  # (5) A little-known feature of Shiny is that the UI can be a function, not just
   # objects. You can use this to dynamically render the UI based on the request.
   # We're going to pass this uiFunc, not ui, to shinyApp(). If you're using
   # ui.R/server.R style files, that's fine too--just make this function the last
   # expression in your ui.R file.
   uiFunc <- function(req) {
     
-    # Check if HTTPs
+    # (6) Check if HTTPs
     if (!interactive()) {
       if (req$HTTP_X_FORWARDED_PROTO != "https") {
-        redirect <- sprintf("location.replace(\"%s\");", APP_URL)
-        tags$script(HTML(redirect)) # redirect if it isn't
+        return("ERROR: Not HTTPS")
       }
     } else {
       print("localhost app, not verifying https")
     }
     
-    # Check if token in URL, else redirect
+    # (7) Check if token in URL, else redirect
     if (!has_auth_code(parseQueryString(req$QUERY_STRING))) {
-      url <- oauth2.0_authorize_url(sfdcAPI, app, scope = scope)
+      url <- oauth2.0_authorize_url(api, app, scope = scope)
       redirect <- sprintf("location.replace(\"%s\");", url)
       tags$script(HTML(redirect))
     } else {
       
-      # Manually create a token (saved to local environment)
+      # (8) Manually create a token (saved to local environment)
       token <<- oauth2.0_token(
         app = app,
-        endpoint = sfdcAPI,
+        endpoint = api,
         credentials = oauth2.0_access_token(
-          sfdcAPI, 
+          api, 
           app, 
           parseQueryString(req$QUERY_STRING)$code
         ),
         cache = FALSE
       )
       
-      # Perform a GET request to confirm access
-      sfdUserInfo <- "https://login.salesforce.com/services/oauth2/userinfo"
-      resp <- GET(sfdUserInfo, config(token = token))
+      # (9) Perform a GET request to confirm access
+      resp <- GET("https://api.github.com/user", config(token = token))
       stop_for_status(resp) # CRASH APP IF ERROR RETURNED
-      json <- jsonlite::fromJSON(content(resp, "text") ,simplifyDataFrame = FALSE)
-      if(json$organization_id %in% jsonlite::fromJSON(Sys.getenv("ORG_ID"))) {
-        return(ui)
-      } else {
-        span(paste("Unauthorized", json$organization_id))
-      }
+      
+      return(ui) # otherwise return UI
     }
   }
   
-  
-  # Note that we're using uiFunc, not ui!
+  # (10) Note that we're using uiFunc, not ui!
   if (interactive()) {
     
     # run locally
-    options(shiny.port = 8100)
     shinyApp(uiFunc, server)
     
   } else {
-    print("run.R runApp about to start")
     
-    # run on heroku w/ nginx
+    # Create a file at the path /tmp/app-initalized
+    # This tells nginx that the app is running, and it can now forward traffic to the custom 'port' below
+    # If this file is created, nginx will simply wait forever until the app says its running
     file.create("/tmp/app-initialized")
     port <- '/tmp/nginx.socket'
     attr(port, 'mask') <- strtoi("117", 8)
     
+    
+    # Runs the app in the working directory. By default this will search for an `app.R` file or 
+    # a ui.R + server.R file combination
+    # In this case the port is now the address to the nginx proxy
     shiny::runApp(
       appDir = shinyApp(uiFunc, server),
       port = port
@@ -141,5 +132,4 @@ RunMyShinyApp <- function() {
   }
 }
 
-# Run it
-RunMyShinyApp()
+RunHerokuApp()
