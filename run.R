a94768732cf0b148d2adec89a63c35192e286b03
Andrew Watson <andrew.watson@salesforce.com>
2018-05-08 03:44:42 -0700

    added nginx

--- run.R
+++ run.R
@@ -6,26 +6,25 @@
 # MIT License and shit
 #
 
-# simple run.R
 library(shiny)
 
-# Gets the port # from Heroku
-port <- Sys.getenv('PORT')
+# When we run nginx we need to change R to point to the /app folder manually
+setwd('/app')
+
+# Create a file at the path /tmp/app-initalized
+# This tells nginx that the app is running, and it can now forward traffic to the custom 'port' below
+# If this file is created, nginx will simply wait forever until the app says its running
+file.create("/tmp/app-initialized")
+port <- '/tmp/nginx.socket'
+attr(port, 'mask') <- strtoi("117", 8)
+
 
 # Runs the app in the working directory. By default this will search for an `app.R` file or 
 # a ui.R + server.R file combination
-# The host of 0.0.0.0 indicates the app runs at the web server's ip address
-
+# In this case the port is now the address to the nginx proxy
 rmarkdown::run(
     'tutorial.Rmd'
   , shiny_args = list(
-      host = '0.0.0.0',
-      port = as.numeric(port)
+      port = port
   )
 )
-
-# shiny::runApp(
-#   appDir = getwd(),
-#   host = '0.0.0.0',
-#   port = as.numeric(port)
-# )
