2e5bfe2fa14c8e1870daeaa77f96d21825cffabc
jcrodriguez1989 <jcrodriguez@unc.edu.ar>
2021-10-07 12:03:26 -0300

    updates for 1.

--- 1. simple unsecured app/run.R
+++ 1. simple unsecured app/run.R
@@ -6,3 +6,17 @@
 # Created by blakiseskream on 5/7/2018
 # MIT License and shit
 #
+
+library(shiny)
+
+# Gets the port # from Heroku
+port <- Sys.getenv("PORT")
+
+# Runs the app in the working directory. By default this will search for an `app.R` file or
+# a ui.R + server.R file combination
+# The host of 0.0.0.0 indicates the app runs at the web server's ip address
+shiny::runApp(
+  appDir = getwd(),
+  host = "0.0.0.0",
+  port = as.numeric(port)
+)
